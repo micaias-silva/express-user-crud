980f34e7d9402231a75ad0b33eff96ab767c9096
Micaías Silva <micaiasilvaop@gmail.com>
2022-09-21 14:31:23 -0300

    Feat: Aplicação consumindo novas rotas

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import express from "express";
-import "dotenv/config"
+import "dotenv/config";
+import routes from "./routes";
 
 const app = express();
 app.use(express.json());
-    
-const PORT = process.env.PORT || 3000   
+app.use("/", routes);
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`server is running at port ${PORT}`)
+  console.log(`server is running at port ${PORT}`);
 });
 
-export default app
+export default app;
