e1ee83ab2e320b96431fa9865ff0a713ab736205
Micaías Silva <micaiasilvaop@gmail.com>
2022-09-22 14:26:20 -0300

    Feat: Rota de login e implementação dos middlewares em suas respectivas rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import createUserController from "../controllers/createUser.controller";
 import getUsersController from "../controllers/getUsersController";
+import loginController from "../controllers/login.controller";
+import verifyAdmAuthMiddleware from "../middlewares/verifyAdmAuth.middleware";
+import verifyEmailAvailability from "../middlewares/verifyEmailAlreadyExists.middleware";
+import verifyUserAuthMiddleware from "../middlewares/verifyUserAuth.middleware";
 
 const routes = Router();
 
-routes.get("/users", getUsersController);
-routes.post("/users", createUserController);
+routes.get("/users", verifyAdmAuthMiddleware, getUsersController);
+routes.post("/users", verifyEmailAvailability, createUserController);
+routes.post("/login", loginController);
 
 export default routes;
