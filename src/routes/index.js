f2b4767f0df31b8f1d6681e9cc6d2d00ffa00e19
Micaías Silva <micaiasilvaop@gmail.com>
2022-09-22 19:04:42 -0300

    Feat: Rotas finais adicionadas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,35 @@
 import { Router } from "express";
 import createUserController from "../controllers/createUser.controller";
+import userProfileController from "../controllers/userProfile.controller";
 import getUsersController from "../controllers/getUsersController";
 import loginController from "../controllers/login.controller";
 import verifyAdmAuthMiddleware from "../middlewares/verifyAdmAuth.middleware";
 import verifyEmailAvailability from "../middlewares/verifyEmailAlreadyExists.middleware";
 import verifyUserAuthMiddleware from "../middlewares/verifyUserAuth.middleware";
+import deleteUserController from "../controllers/deleteUser.controller";
+import verifyAuthFlowMiddleware from "../middlewares/verifyAuthFlow.middleware";
+import updateUserDataController from "../controllers/updateUserData.controller";
 
 const routes = Router();
 
 routes.get("/users", verifyAdmAuthMiddleware, getUsersController);
+routes.get("/users/profile", verifyUserAuthMiddleware, userProfileController);
+
 routes.post("/users", verifyEmailAvailability, createUserController);
 routes.post("/login", loginController);
+
+routes.delete(
+  "/users/:uuid",
+  verifyUserAuthMiddleware,
+  verifyAuthFlowMiddleware,
+  deleteUserController
+);
+
+routes.patch(
+  "/users/:uuid",
+  verifyUserAuthMiddleware,
+  verifyAuthFlowMiddleware,
+  updateUserDataController
+);
 
 export default routes;
